17cb19f06daaff774a2b91b31e894c4d6ef30ede
FernandaMonsalveLuco <fernandamonsalveluco@gmail.com>
2025-08-21 15:37:10 -0400

    Delete src directory
    Signed-off-by: FernandaMonsalveLuco <fernandamonsalveluco@gmail.com>

--- src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import ordenReducer from "../features/ordenSlice";
-
-export const store = configureStore({
-  reducer: {
-    orden: ordenReducer,
-  },
-});
